b33bcd3ae2bf08c4ac283f52f29021cb60747c40
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2022-09-09 16:03:03 +0100

    save

---
+++ aula6-variaveis/index.js
@@ -0,0 +1,21 @@
+let nome;   //Declarou a variável
+nome = 'Athos'; //Inicializando a variável
+
+let carro = 'Ford'; // Declarou E inicializou a variável na mesma linha
+
+console.log(nome, 'nasceu em 2025.');
+console.log('Em 2050', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2055.');
+console.log('Maria teve um filho com', nome, 'em 2056.');
+console.log('O filho de', nome, 'se chama Iuri.');
+
+
+// REGRAS DAS VARIÁVEIS
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços 
+// Usa camelCase = primera palavra minuscula e a sucessivas maiusculas. Ex: nomeCliente
+// Case-sensitive = letras minusculas e maiusculas fazem diferença
+// NÃO UTILIZE VAR, UTILIZE LET.
