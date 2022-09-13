f460ac0e8919f8a60c15a0dc0595becb8b06319a
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2022-09-13 23:02:25 +0100

    att

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -1,4 +1,4 @@
-let num1 = prompt('Digite um número:');
+/*let num1 = prompt('Digite um número:');
 let num2 = prompt('Digite outro número:');
 
 
@@ -9,3 +9,24 @@ const resultado = num1 + num2;
 
 
 alert('O resultado da soma é: ' + resultado + '.');
+
+
+
+let nome = prompt('Diga seu primeiro nome:');
+let nome2 = prompt('Agora diga seu segundo nome:');
+
+const resultado2 = nome + nome2;
+
+alert('Seu nome é: '+ nome + ' ' + nome2);
+*/
+
+
+let anoNascimento = prompt('Me diga seu ano de nascimento:');
+let anoAtual = 2022;
+
+anoAtual = Number(anoAtual);
+
+const idade = anoAtual - anoNascimento;
+
+
+alert('UAU, voce tem ' + idade + ' !! ' + 'Tá ficando velho kakaka.');
