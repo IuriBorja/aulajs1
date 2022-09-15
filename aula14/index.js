e015f8106873f7d5598c45ccfcc1be374eca221c
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2022-09-15 01:28:44 +0100

    att

---
+++ aula14/index.js
@@ -0,0 +1,11 @@
+let num1 = 1; // Number
+let num2 = 2.5; // Number
+
+
+// console.log(num1.toStrong() + num2);
+
+num1 = num1.toString();
+console.log(typeof num1);
+
+// para trasnformar um numero em uma string é usar o metodo .toString();
+
---
+++ aula15/index.js
@@ -0,0 +1,25 @@
+let num1 = 9;
+
+console.log(100 / 0); // no js é possivel dividir por zero, que resulta
+// no valor infinity, com valor true
+
+// console.log(num1 ** 0.5); // método de como descobrir a raiz do numero
+
+
+
+// let num2 = Math.floor(num1); // arredonda um numero para baixo
+
+// let num2 = Math.ceil(num1); // arredonda um numero para cima
+
+// let num2 = Math.round(num1); // arredonda de forma automatica
+
+//console.log(Math.max(1,2,3,4,-10,1500,8,7,6,5)); // pega o maior numero
+//numa lista de numeros
+
+//console.log(Math.min(1,2,3,4,-10,1500,8,7,6,5)); // pega o menor numero
+//numa lista de numeros
+
+
+const aleatorio = Math.floor(Math.random() * 10);
+
+console.log(aleatorio);
