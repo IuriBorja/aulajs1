3d9272f676720080bc8cb077dc390209a431f242
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2022-09-28 02:28:44 +0100

    ternari-operator

---
+++ aula27-operador-ternario/index.js
@@ -0,0 +1,22 @@
+const random = Math.floor(Math.random() * 11);
+
+console.log(random);
+
+const resultRandom = random >= 5 ? 'User VIP' : 'Normal user'; // isso é
+// a mesma coisa que escrever o if de baixo 
+
+
+/*
+
+    if (random >= 5) {
+        console.log('User VIP');
+    } else {
+        console.log('Normal user');
+    }
+ 
+ */
+
+
+
+
+console.log(resultRandom);
