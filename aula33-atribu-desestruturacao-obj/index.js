a24b97ee81ffe66598491102ed5edad630960e30
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2022-09-29 16:22:09 +0100

    att

---
+++ aula33-atribu-desestruturacao-obj/index.js
@@ -0,0 +1,29 @@
+//EXEMPLO DE COMO USAR OBJETOS COM ARRAYS
+
+/*
+const numeros = {
+    inteiros: [1,2,3,4,5,6],
+    reais: [1.1,2.2,3.3,4.4,5.5]
+};
+ 
+const {inteiros: [um,,tres,...restoInteiros], reais: [umReal,,tresReal,...restoReais]} = numeros
+ 
+console.log(um,tres,restoInteiros,umReal,tresReal,restoReais);
+*/
+
+//---------------------------------------
+
+
+const pessoa = {
+    nome: 'Iuri',
+    sobrenome: 'Borja',
+    idade: 32,
+    endereco: {
+        rua: 'Av Brasil',
+        numero: 106
+    }
+};
+
+//Atribuição via desestruturação
+const {nome, sobrenome, idade} = pessoa;
+console.log(nome, sobrenome);
