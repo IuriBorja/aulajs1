27ff8c674e84ccdcdfb008755294709e80f16853
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2022-10-04 23:05:09 +0100

    save

---
+++ modelo-exercicios-futuros copy 3/assets/js/main.js
@@ -0,0 +1,15 @@
+const caixa = document.querySelector('.paragrafos');
+//caixa.style.background = 'rgb(17, 86, 102)';
+//caixa.style.color = 'white';
+
+const ps = caixa.querySelectorAll('p');
+
+
+for (let p of ps) {
+    p.style.background = 'rgb(17, 86, 102)';
+    p.style.color = 'white';
+}
+
+
+
+//color:rgb(17, 86, 102)
