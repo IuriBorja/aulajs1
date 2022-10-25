85139115ed1718e14ac093610b2417b987c3abfb
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2022-10-25 20:14:22 -0300

    save

---
+++ aula67-reduce-array/aula67.js
@@ -0,0 +1,14 @@
+//REDUCE é mais utilizada para reduzir o array em um único elemento
+
+
+
+//exemplo
+
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11];
+
+const total = numeros.reduce(function(acumulador, valor, indice, array) {
+    
+}, 0);
+
+
